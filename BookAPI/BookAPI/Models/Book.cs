90d57c1387e24967c38ebaf612d11cb98f1c2714
tamanazehra <zehratamana3@gmail.com>
2025-05-06 15:49:06 +0530

    Book-API

--- BookAPI/BookAPI/Models/Book.cs
+++ BookAPI/BookAPI/Models/Book.cs
@@ -1,8 +1,11 @@
-﻿namespace BookAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookAPI.Models
 {
     public class Book
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Author { get; set; }
         public DateTime PublishedYear { get; set; }
--- BookAPI/BookAPI/obj/Debug/net8.0/BookAPI.AssemblyInfo.cs
+++ BookAPI/BookAPI/obj/Debug/net8.0/BookAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4dd2ce6f661cd875e095a583b50490b12c2ccc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776ad6a12ad4ec0732a303b2ac8c40b3802923e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
